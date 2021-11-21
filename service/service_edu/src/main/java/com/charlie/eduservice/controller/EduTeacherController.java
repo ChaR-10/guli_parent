78ab75dd1f8c705cca66ff59df36b66b42554d4c
Char <char@gmail.com>
2021-11-21 12:54:11 +0800

    feat:mp生成EduTeacher

---
+++ service/service_edu/src/main/java/com/charlie/eduservice/controller/EduTeacherController.java
@@ -0,0 +1,21 @@
+package com.charlie.eduservice.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 讲师 前端控制器
+ * </p>
+ *
+ * @author testjava
+ * @since 2021-11-21
+ */
+@RestController
+@RequestMapping("/eduservice/teacher")
+public class EduTeacherController {
+
+}
+
---
+++ service/service_edu/src/main/java/com/charlie/eduservice/service/EduTeacherService.java
@@ -0,0 +1,16 @@
+package com.charlie.eduservice.service;
+
+import com.charlie.eduservice.entity.EduTeacher;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 讲师 服务类
+ * </p>
+ *
+ * @author testjava
+ * @since 2021-11-21
+ */
+public interface EduTeacherService extends IService<EduTeacher> {
+
+}
