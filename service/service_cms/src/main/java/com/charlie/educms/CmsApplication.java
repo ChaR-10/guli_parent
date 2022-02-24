31d8a422b4f523140757d97b1ab1a111b2536add
Char <char@gmail.com>
2022-02-24 22:29:48 +0800

    feat：cms添加nacos服务注册配置配置

--- service/service_cms/src/main/java/com/charlie/educms/CmsApplication.java
+++ service/service_cms/src/main/java/com/charlie/educms/CmsApplication.java
@@ -3,6 +3,7 @@ package com.charlie.educms;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan({"com.charlie"})//指定扫描位置
 @MapperScan("com.charlie.educms.mapper")
+@EnableDiscoveryClient
 public class CmsApplication {
 
     public static void main(String[] args) {
