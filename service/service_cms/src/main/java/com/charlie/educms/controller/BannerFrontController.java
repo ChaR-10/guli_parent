b3f5c69e4a6ac07ebc7f578f876e0513949901d5
Char <char@gmail.com>
2022-02-26 17:56:33 +0800

    feat：1 添加redis配置; 2 查询所有Banner进行redis缓存

--- service/service_cms/src/main/java/com/charlie/educms/controller/BannerFrontController.java
+++ service/service_cms/src/main/java/com/charlie/educms/controller/BannerFrontController.java
@@ -6,6 +6,7 @@ import com.charlie.educms.service.CrmBannerService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +27,7 @@ public class BannerFrontController {
     @Autowired
     private CrmBannerService bannerService;
 
+    @Cacheable(value = "banner",key = "'selectIndexList'")
     @ApiOperation(value = "查询所有banner")
     @GetMapping("getAllBanner")
     public R getAllBanner() {
