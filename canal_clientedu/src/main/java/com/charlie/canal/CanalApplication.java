541541cedad608f82d33dd315b3bad935062c809
Char <char@gmail.com>
2022-04-10 14:25:03 +0800

    feat: Canal数据同步工具

---
+++ canal_clientedu/src/main/java/com/charlie/canal/CanalApplication.java
@@ -0,0 +1,30 @@
+package com.charlie.canal;
+
+import com.charlie.canal.client.CanalClient;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author: ChaR
+ * @Date: 2022/4/9 18:55
+ */
+@SpringBootApplication
+public class CanalApplication implements CommandLineRunner {
+
+    @Resource
+    private CanalClient canalClient;
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanalApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        //项目启动，执行canal客户端监听
+        canalClient.run();
+    }
+}
+
