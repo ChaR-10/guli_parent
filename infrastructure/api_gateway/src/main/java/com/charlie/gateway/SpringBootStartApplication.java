1dfb8c6393b27469dfd0e70e359f83b997f48d85
ChaR <char@gmail.com>
2022-04-16 19:19:31 +0800

    fix：网关模块取消war包打包

---
+++ infrastructure/api_gateway/src/main/java/com/charlie/gateway/SpringBootStartApplication.java
@@ -0,0 +1,17 @@
+//package com.charlie.gateway;
+//
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//
+///**
+// * @Author: ChaR
+// * @Date: 2022/4/14 13:03
+// */
+//public class SpringBootStartApplication extends SpringBootServletInitializer {
+//
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(ApiGatewayApplication.class);
+//    }
+//}
